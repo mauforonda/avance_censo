29482877b6d2b899044ced6fc963f92025d2e78c
Mauricio Foronda <josemauricioforonda@gmail.com>
2022-08-21 20:41:38 -0400

    fix: convocatorias sin fecha de publicación pero sí de límite

--- update/convocatorias.py
+++ update/convocatorias.py
@@ -9,19 +9,22 @@ def get_convocatorias():
     
     URL = "https://wsbt.ine.gob.bo/api/public/convocatorias/publicas/todos"
     tabla = pd.read_json(URL)
-    tabla = tabla[['fecha_publicacion', 'referencia', 'num_vacancia']]
+    tabla = tabla[['fecha_publicacion', 'fecha_fin_conv', 'referencia', 'num_vacancia']]
     convocatorias = []
     for i, row in tabla.iterrows():
         for depto in row['num_vacancia'].split(','):
             demanda = int(depto.split(' ')[0])
             nombre = '_'.join(depto.strip().split(' ')[1:])
-            convocatorias.append({'fecha_publicacion':row['fecha_publicacion'], 'departamento': nombre, 'referencia': row['referencia'], 'demanda': demanda})
+            convocatorias.append({'fecha_publicacion':row['fecha_publicacion'], 'fecha_limite':row['fecha_fin_conv'], 'departamento': nombre, 'referencia': row['referencia'], 'demanda': demanda})
             
     convocatorias = pd.DataFrame(convocatorias)
     for col in ['departamento', 'referencia']:
         convocatorias[col] = convocatorias[col].str.lower().apply(lambda i: unicodedata.normalize('NFKD', i).encode('ascii', 'ignore').decode('ascii').replace(' ', '_'))
-
-    convocatorias = convocatorias.sort_values(['fecha_publicacion', 'departamento', 'referencia'])
+    
+    for col in ['fecha_publicacion', 'fecha_limite']:
+        convocatorias[col] = pd.to_datetime(convocatorias[col]).dt.date
+        
+    convocatorias = convocatorias.sort_values(['fecha_limite', 'departamento', 'referencia'])
     total = convocatorias.groupby(['departamento', 'referencia']).demanda.sum().reset_index().pivot_table(index='departamento', columns='referencia', values='demanda').fillna(0).astype(int)
 
     return total, convocatorias
