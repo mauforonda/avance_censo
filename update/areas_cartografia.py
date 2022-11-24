06e0544846b0bf2da690293a62a6dde34d706e5e
Mauricio Foronda <josemauricioforonda@gmail.com>
2022-11-24 09:59:01 -0400

    fail if you want, just print the error message

--- update/areas_cartografia.py
+++ update/areas_cartografia.py
@@ -13,26 +13,32 @@ DIRECTORY = 'data/cartografia'
 URL = "https://sigedv2.ine.gob.bo/geoserver/ows?service=WFS&request=GetFeature&typeName=seguimiento:vw_ac_2021_cartografia&outputFormat=csv"
 RELEVANT_COLUMNS = ["codigo","sectores_ace","sector"]
 
-ayer = dt.datetime.now(tz=pytz.timezone('America/La_Paz')).replace() - dt.timedelta(days=1)
-df = pd.read_csv(URL)[RELEVANT_COLUMNS]
-df.insert(0, 'fecha', ayer)
+def update():
+    ayer = dt.datetime.now(tz=pytz.timezone('America/La_Paz')).replace() - dt.timedelta(days=1)
+    df = pd.read_csv(URL)[RELEVANT_COLUMNS]
+    df.insert(0, 'fecha', ayer)
 
-total = df[['codigo', 'sectores_ace']]
-total.to_csv(
-    f'{DIRECTORY}/areas_totales.csv',
-    header=['codigo_municipio', 'areas'],
-    index=False
-)
+    total = df[['codigo', 'sectores_ace']]
+    total.to_csv(
+        f'{DIRECTORY}/areas_totales.csv',
+        header=['codigo_municipio', 'areas'],
+        index=False
+    )
 
-avance_filename = f'{DIRECTORY}/areas_concluidas_o_en_proceso.csv'
-avance = df[['fecha', 'codigo', 'sector']]
-avance.columns = ['fecha', 'codigo_municipio', 'areas']
-avance = pd.concat([
-    pd.read_csv(avance_filename, parse_dates=['fecha']),
-    avance
-])
-avance.fecha = avance.fecha.apply(lambda x: x.strftime('%Y-%m-%d'))
-avance.to_csv(
-    avance_filename,
-    index=False
-)
+    avance_filename = f'{DIRECTORY}/areas_concluidas_o_en_proceso.csv'
+    avance = df[['fecha', 'codigo', 'sector']]
+    avance.columns = ['fecha', 'codigo_municipio', 'areas']
+    avance = pd.concat([
+        pd.read_csv(avance_filename, parse_dates=['fecha']),
+        avance
+    ])
+    avance.fecha = avance.fecha.apply(lambda x: x.strftime('%Y-%m-%d'))
+    avance.to_csv(
+        avance_filename,
+        index=False
+    )
+
+try:
+    update()
+except Exception as e:
+    print(e)
